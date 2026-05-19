2e4a3c7c2c5d5d212bc00aea6c49fe328f44bc77
xusninuur <xusninuur@gamail.com>
2026-05-19 11:21:07 +0300

    add index and styles

--- server.js
+++ server.js
@@ -4,24 +4,31 @@ const dns = require("dns");
 require("dotenv").config();
 
 const app = express();
+
 dns.setServers(["1.1.1.1"]);
 
 // Middleware
 app.use(express.json());
 
-// Routes
 const productRoutes = require("./routes/productRoutes");
 app.use("/api", productRoutes);
 
-// MongoDB connection
+app.get("/", (req, res) => {
+  res.send("PRODUCTS API");
+});
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB Error:", err.message));
 
-// Server start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+// Right now server is running on port 3000,
+// but when hosting on a platform like Render, Heroku,
+// it will use the random port provided by the hosting platform
+// or default to 3000 if the PORT variable is not set (e.g. when running locally)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port : ${PORT}`);
 });
